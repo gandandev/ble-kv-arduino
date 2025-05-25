dfe62bcd00ac066c9150ae784682b92880c2b5ad
gandan <188059221+gandandev@users.noreply.github.com>
2025-05-25 19:43:50 +0900

    chore: set up arduino library project

---
+++ src/BleKv.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file BleKv.cpp
+ * @brief Implementation of BleKv class for key-value messaging over BLE
+ * @author gandandev <gandan.dev@gmail.com>
+ * @version 0.0.0
+ */
+
+#include "BleKv.h"
---
+++ src/BleKv.h
@@ -0,0 +1,18 @@
+/**
+ * @file BleKv.h
+ * @brief Key-value messaging over BLE for HM-10 and similar modules
+ * @author gandandev <gandan.dev@gmail.com>
+ * @version 0.0.0
+ *
+ * Seamless key-value BLE messaging between browser and Arduino.
+ * For HM-10 and similar modules.
+ *
+ * @see https://github.com/gandandev/ble-kv
+ */
+
+#ifndef BLE_KV_H
+#define BLE_KV_H
+
+#include <Arduino.h>
+
+#endif // BLE_KV_H
